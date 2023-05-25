1b125622352d59209c16d7026625fc8b6a2d94d7
skybluelion <brotherjun96@gmail.com>
2023-05-25 13:39:05 +0900

    [Feat] #24 Review 기능 수정

--- src/main/java/com/spacebetween/linket/controller/ReviewController.java
+++ src/main/java/com/spacebetween/linket/controller/ReviewController.java
@@ -33,14 +33,22 @@ public class ReviewController {
     /*
         리뷰 등록 API
      */
-    @PostMapping("/member/review")
-    public ResponseEntity<ReviewJoinDto> registerReview(@RequestBody ReviewJoinDto reviewJoinDto) throws Exception{
-        int rowCnt = reviewService.registerReview(reviewJoinDto);
-        System.out.println(rowCnt);
-        if(rowCnt==1) // 리뷰 등록 성공
-            return new ResponseEntity<>(reviewJoinDto, header, HttpStatus.OK);
-        else // 등록 실패
-            return new ResponseEntity<>(null, header, HttpStatus.BAD_REQUEST);
+
+    @PostMapping("/member/{ticketId}/review")
+    public ResponseEntity<ReviewJoinDto> registerReview(@RequestBody ReviewJoinDto reviewJoinDto,@PathVariable Long ticketId) throws Exception{
+        int cnt = reviewService.checkReview(ticketId);
+        if(cnt==0) {// 해당하는 티켓의 리뷰 존재하지 않음.
+            int rowCnt = reviewService.registerReview(reviewJoinDto);
+            System.out.println(rowCnt);
+            if(rowCnt==1) // 리뷰 등록 성공
+                return new ResponseEntity<>(reviewJoinDto, header, HttpStatus.OK);
+            else // 등록 실패
+                return new ResponseEntity<>(null, header, HttpStatus.BAD_REQUEST);
+        }else {// 리뷰 존재
+            ReviewJoinDto result = new ReviewJoinDto();
+            return new ResponseEntity<>(result, header, HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @GetMapping("/staff/{eventId}/status")
@@ -53,17 +61,7 @@ public class ReviewController {
         }
     }
 
-    @GetMapping("/member/{ticketId}/check")
-    public ResponseEntity<String> checkReview(@PathVariable Long ticketId) throws Exception {
-        int cnt = reviewService.checkReview(ticketId);
-        if(cnt==0) {// 리뷰 존재하지 않음.
-            String result = "리뷰 작성 가능";
-            return new ResponseEntity<>(result, header, HttpStatus.OK);
-        }else {// 리뷰 존재
-            String result = "해당 티켓은 이미 리뷰가 존재합니다.";
-            return new ResponseEntity<>(result, header, HttpStatus.BAD_REQUEST);
-        }
-    }
+
 
     @GetMapping("/member/reviews")
     public ResponseEntity<List<Map<String, Object>>> getAllReviews(HttpSession session) throws Exception{
