773224b17fe08570b4713913eceed71f3e3a7d70
Do Yeon Park <ehduszkdzkd@naver.com>
2023-05-25 18:00:48 +0900

    [Feature/User] #5 User 기능 개발

--- src/test/java/com/spacebetween/linket/dao/user/UserDAOImplTest.java
+++ src/test/java/com/spacebetween/linket/dao/user/UserDAOImplTest.java
@@ -18,49 +18,49 @@ public class UserDAOImplTest {
     @Autowired
     private UserDAO userDAO;
 
-    @Test
-    public void getUser() throws Exception{
-        UserJoinDto userJoinDto = userDAO.getUser("aaa@aaa.com");
-        System.out.println(userJoinDto);
-        assertTrue(userJoinDto!=null);
-    }
-
-    @Test
-    public void checkUserPwd() throws Exception{
-        HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("email","aaa@aaa.com");
-        hashMap.put("password","1234");
-
-        UserJoinDto userJoinDto = userDAO.checkUserPwd(hashMap);
-        System.out.println(userJoinDto);
-        assertTrue(userJoinDto!=null);
-    }
-
-    @Test
-    public void updateUserPhone() throws Exception{
-        HashMap<String,String> hashMap = new HashMap<>();
-        hashMap.put("email", "aaa@aaa.com");
-        hashMap.put("phone","010-2222-2222");
-
-        int rowCnt = userDAO.updateUserPhone(hashMap);
-
-        UserJoinDto userJoinDto = userDAO.getUser("aaa@aaa.com");
-        System.out.println(userJoinDto);
-
-        assertTrue(rowCnt==1);
-    }
-
-    @Test
-    public void updateUserPassword() throws Exception{
-        HashMap<String,String> hashMap = new HashMap<>();
-        hashMap.put("email", "aaa@aaa.com");
-        hashMap.put("password","6789");
-
-        int rowCnt = userDAO.updateUserPassword(hashMap);
-
-        UserJoinDto userJoinDto = userDAO.getUser("aaa@aaa.com");
-        System.out.println(userJoinDto);
-
-        assertTrue(rowCnt==1);
-    }
+//    @Test
+//    public void getUser() throws Exception{
+//        UserJoinDto userJoinDto = userDAO.getUser("aaa@aaa.com");
+//        System.out.println(userJoinDto);
+//        assertTrue(userJoinDto!=null);
+//    }
+//
+//    @Test
+//    public void checkUserPwd() throws Exception{
+//        HashMap<String, String> hashMap = new HashMap<>();
+//        hashMap.put("email","aaa@aaa.com");
+//        hashMap.put("password","1234");
+//
+//        UserJoinDto userJoinDto = userDAO.checkUserPwd(hashMap);
+//        System.out.println(userJoinDto);
+//        assertTrue(userJoinDto!=null);
+//    }
+//
+//    @Test
+//    public void updateUserPhone() throws Exception{
+//        HashMap<String,String> hashMap = new HashMap<>();
+//        hashMap.put("email", "aaa@aaa.com");
+//        hashMap.put("phone","010-2222-2222");
+//
+//        int rowCnt = userDAO.updateUserPhone(hashMap);
+//
+//        UserJoinDto userJoinDto = userDAO.getUser("aaa@aaa.com");
+//        System.out.println(userJoinDto);
+//
+//        assertTrue(rowCnt==1);
+//    }
+//
+//    @Test
+//    public void updateUserPassword() throws Exception{
+//        HashMap<String,String> hashMap = new HashMap<>();
+//        hashMap.put("email", "aaa@aaa.com");
+//        hashMap.put("password","6789");
+//
+//        int rowCnt = userDAO.updateUserPassword(hashMap);
+//
+//        UserJoinDto userJoinDto = userDAO.getUser("aaa@aaa.com");
+//        System.out.println(userJoinDto);
+//
+//        assertTrue(rowCnt==1);
+//    }
 }
